61ce40e3f3cec7f395e8241ddda5d815d686b2f5
solyanni4 <solyannikow93@gmail.com>
2020-11-29 13:14:14 +0300

    Revert "test67"
    This reverts commit 0c0869808e569e6c92518bfd42ad45e656036c58.

--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-const Header = () => {
-    return (
-        <header className='Header'>
-            <img src='https://icon2.kisspng.com/20180418/dqe/kisspng-computer-icons-database-enterprise-search-informat-transfer-5ad7c418528714.8569715215240898803381.jpg'></img>;
-        </header>
-    );
-}
-export default Header;
