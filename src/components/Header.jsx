687b03461d21e61700f519a836f1f1dad1a03b1b
solyanni4 <solyannikow93@gmail.com>
2020-12-03 12:52:26 +0300

    css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
-import './Header.css';
+import s from './Header.module.css';
 
 const Header = () => {
-    return (
-        <header className='Header'>
+    return <header className={s.header}>
             <img src='https://icon2.kisspng.com/20180418/dqe/kisspng-computer-icons-database-enterprise-search-informat-transfer-5ad7c418528714.8569715215240898803381.jpg'></img>;
         </header>
-    );
 }
 export default Header;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
-import './Profile.css';
+import s from './Profile.module.css';
+
 const Profile = () => {
     return (
-        < div className='content' >
+        < div className={s.content} >
             <img src="https://cs6.livemaster.ru/storage/c2/ee/140a757951f68c5021935ae1850k.jpg"></img>;
             <div>
                 ava+description
@@ -11,13 +12,16 @@ const Profile = () => {
         <div>
                     News posts
         </div>
-        <div className ='item'>
-                    post1
-        </div>
-        <div className ='item'>
-                    post2
-        </div>
-     </div>
+                <div className={s.posts}>
+                    <div className={s.item}>
+                        post1
+                </div>
+                    <div className={s.item}>
+                        post2
+                </div>
+                </div>
+            </div>
+
         </div >);
 }
 export default Profile;
